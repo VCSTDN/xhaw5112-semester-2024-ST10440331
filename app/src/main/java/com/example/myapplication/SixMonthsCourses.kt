e868e913c1a6e433c59280eddb2a456240f6a8a1
ST10440331 <161335488+ST10440331@users.noreply.github.com>
2024-08-16 12:36:36 +0200

    Commit Message

---
+++ app/src/main/java/com/example/myapplication/SixMonthsCourses.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SixMonthsCourses : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_six_months_courses)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val sewingBtn: Button = findViewById(R.id.sewingBtn)
+        val sewingpictureBtn: ImageButton = findViewById(R.id.sewingpictureBtn)
+
+        val landBtn: Button = findViewById(R.id.landBtn)
+        val firstBtn: Button = findViewById(R.id.firstBtn)
+        val lifeBtn: Button = findViewById(R.id.lifeBtn)
+        val sixweekBtn: Button = findViewById(R.id.sixweekBtn)
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/SixWeekCourses.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.model.ButtCap
+
+class SixWeekCourses : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_six_week_courses)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val childBtn: Button = findViewById(R.id.childBtn)
+        val cookingBtn:Button = findViewById(R.id.cookingBtn)
+        val gardenBtn: Button = findViewById(R.id.gardenBtn)
+        val sixweekBtn: Button = findViewById(R.id.sixweekBtn)
+
+    }
+}
